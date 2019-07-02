a636b84f3956f68b7b38ed4f3ae00d1a23901902
Lisa Carbonell <morekaleplease@gmail.com>
2019-07-02 15:13:54 -0700

    components UserSignIn.js pseudo code

---
+++ client/src/components/UserSignIn.js
@@ -0,0 +1,7 @@
+// provides the "Sign In" screen
+// rendering a form that allows a user to sign using their existing account information
+// component also renders a "Sign In" button 
+// when clicked signs in the user 
+// a "Cancel" button that returns the user to the default route (i.e. the list of courses).
+
+
