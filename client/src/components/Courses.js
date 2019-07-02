d0292efdb77d19108a1533d39296a6262ecc7422
Lisa Carbonell <morekaleplease@gmail.com>
2019-07-02 14:27:48 -0700

    components courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -3,35 +3,47 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 class Courses extends Component { // setup empty courses array
-  state = {
+  constructor(props){
+    super(props)
+    this.state = {
     courses: []
-  };
+    };
+  }
 
+componentDidMount() { // get list of courses from REST API 
+  axios.get("http://localhost:5000/api/courses")
+  .then (response => {
+    this.setState({
+      courses: res.data
+    })
+  })
+}
 
-componentDidMount() {
-  this.getCourses();
-};
+render() { //maps over courses and creates link to course's "Course Detail" screen and link to "Create Course" screen
+        return (
+          <div>
+            <hr />
+            <div className="bounds">
+              {this.state.courses.map((course, index) => ( 
+                <div className="grid-33" key={index}>
+                  <Link className="course--module course--link" to={"/courses/" + course.id}>
+                  <h4 className="course--label">Course</h4>
+                  <h3 className="course--title">{course.title}</h3>
+                  </Link>
+                </div>
+              ))}
 
-// get list of courses from REST API /api/courses route using axios
-// link course to it's Course Detail screen and link to "Create Course" screen
-
-
-var NewComponent = React.createClass({
-    render: function() {
-      return (
-        <div>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="stylesheet" type="text/css" />
-          <link href="https://fonts.googleapis.com/css?family=Cousine" rel="stylesheet" type="text/css" />
-          <link href="../styles/global.css" rel="stylesheet" />
-          <title>Courses</title>
-        </div>
-      );
-    }
-  });
-
-};
+              <div className="grid-33">
+                <Link className="course--module course--link" to="/courses/create">
+                  <h4 className="course--label">Course</h4>
+                  <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
+                      <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " />
+                    </svg>New Course</h3>
+                </Link></div>
+            </div>
+          </div>
+        );
+      }
+    };
 
 export default Courses;
