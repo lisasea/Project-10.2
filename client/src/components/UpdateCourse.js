47ba1b645fae79afb4c6d397ea14c5945fec1de4
Lisa Carbonell <morekaleplease@gmail.com>
2019-07-03 06:56:12 -0700

    UpdateCourse~~~

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -54,13 +54,54 @@ class UpdateCourse extends Component {
       };
 
       render() {
-          const { id, title, description, estimatedTime, materialsNeeded, validationErrors } = this.state;
+          const { user, id, title, description, estimatedTime, materialsNeeded, validationErrors } = this.state;
           return(
-
-
-
-          )
+            <div className="bounds course--detail">
+                <h1>Update Course</h1>
+                <div>
+                {validationErrors ? (
+                  <div>
+                    <h2 className="validation--errors--label"> Validation Errors</h2>
+                    <div className="validation--errors">
+                        <ul>
+                            <li>{validationErrors}</li>
+                        </ul>
+                    </div> 
+                  </div> 
+                ) : ""}
+              <form onSubmit={ this.handleSubmit }>
+                <div className="grid-66">
+                  <div className="course--header">
+                    <h4 className="course--label">Course</h4>
+                    <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." defaultValue={course.title} onChange={e => this.handleInputChange(e)}/></div>
+                    <p>By {user.firstName} {user.lastName}</p>
+                  </div>
+                  <div className="course--description">
+                    <div><textarea id="description" name="description" className placeholder={this.state.course.description} onChange={e => this.handleInputChange(e)}/></div>
+                  </div>
+                </div>
+                <div className="grid-25 grid-right">
+                  <div className="course--stats">
+                    <ul className="course--stats--list">
+                      <li className="course--stats--list--item">
+                        <h4>Estimated Time</h4>
+                        <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" defaultValue={this.state.course.estimatedTime} onChange={e =>this.handleInputChange(e)}/></div>
+                      </li>
+                      <li className="course--stats--list--item">
+                        <h4>Materials Needed</h4>
+                        <div><textarea id="materialsNeeded" name="materialsNeeded" className placeholder="Materials Needed" defaultValue={this.state.course.materialsNeeded} onChange={e => this.handleInputChange(e)}/></div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="grid-100 pad-bottom"><button className="button" type="submit">Update Course</button><button className="button button-secondary" onclick={this.handleCancel}>Cancel</button></div>
+              </form>
+            </div>
+          </div>
+        );
       }
+    };
+
 
 // This component provides the "Update Course" screen
 //  rendering a form that allows a user to update one of their existing courses.
