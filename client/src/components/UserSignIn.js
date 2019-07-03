a8fbc078aa9a40c5534dfd98a392732f4ed2b91e
Lisa Carbonell <morekaleplease@gmail.com>
2019-07-02 20:24:15 -0700

    UserSignIn.js ~~

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -13,7 +13,7 @@ class UserSignIn extends Component {
   }   
    
   handleSubmit = event => {
-    console.log( this.state)
+    console.log(this.state)
     event.preventDefault();
     let userInfo = {"password": this.state.password, "emailAddress": this.state.emailAddress} 
     this.props.signIn(userInfo)
@@ -27,32 +27,27 @@ class UserSignIn extends Component {
 
     render() {
         return ( 
-            var NewComponent = React.createClass({
-                render: function() {
-                  return (
+            <div>
+                <hr/>
+                <div className="bounds">
+                  <div className="grid-33 centered signin">
+                    <h1>Sign In</h1>
                     <div>
-                      <hr />
-                      <div className="bounds">
-                        <div className="grid-33 centered signin">
-                          <h1>Sign In</h1>
-                          <div>
-                            <form>
-                              <div><input id="emailAddress" name="emailAddress" type="text" className placeholder="Email Address" defaultValue /></div>
-                              <div><input id="password" name="password" type="password" className placeholder="Password" defaultValue /></div>
-                              <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
-                            </form>
-                          </div>
-                          <p>&nbsp;</p>
-                          <p>Don't have a user account? <a href="sign-up.html">Click here</a> to sign up!</p>
-                        </div>
-                      </div>
+                        <form onSubmit={this.handleSubmit}>
+                            <div><input id="emailAddress" name="emailAddress" type="text" onChange={this.handleChange} placeholder="Email Address"/></div>
+                            <div><input id="password" name="password" type="password" onChange={this.handleChange} placeholder="Password"/></div>
+                            <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><Link className="button button-secondary" to="/">Cancel</Link></div>
+                        </form>
                     </div>
-                  );
-                }
-              });
-     )
-   } 
+                    <p>&nbsp;</p>
+                    <p>Don't have a user account? <Link to="/sign-up.html">Click here</Link> to sign up!</p>
+                  </div>
+                </div>
+            </div>
+        );
+    }
 }
+     
 export default UserSignIn;
 
 
