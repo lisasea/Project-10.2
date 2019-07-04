764acf08d36159143a5bd2b9df24e406dc8a94b0
Lisa Carbonell <morekaleplease@gmail.com>
2019-07-03 20:44:54 -0700

    new api folder

--- api 2/routes/indexes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", function(req, res, next) {  // GET user
-    res.redirect("/api/users")
-});
-
-module.exports = router;
